ca0b4225172112affe8e448a948223f9bf1fdbb4
Knight6azer <tiwari.ujjwal10c27@gmail.com>
2026-07-18 13:32:09 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Turbopack is enabled by default in Next 16
+  turbopack: {},
 };
 
 export default nextConfig;
